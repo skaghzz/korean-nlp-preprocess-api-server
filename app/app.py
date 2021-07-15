7b8cf8d51f15239bb2b3dfb355c7ea9ed266edf6
namhokim <skaghzz@naver.com>
2021-07-15 17:50:36 +0900

    정규식 기반 텍스트 정재 함수 api 개발

--- app/app.py
+++ app/app.py
@@ -1,25 +1,31 @@
 from flask import Flask, request
-import logging
 
 from modules.preprocess import split_sentence as splitter
+from modules.preprocess import clean_text as cleaner
 
 app = Flask(__name__)
+
+
+def response(result) -> dict:
+    return {
+        'success': True,
+        'error': None,
+        'result': result
+    }
 
 
 @app.route("/split-sentence", methods=['POST'])
 def split_sentence() -> dict:
     data = request.get_json()
-    print(type(data['paragraphs']))
-    logging.debug(data)
     sentence_list = splitter.split_sentence(data['paragraphs'])
-    response = {
-        'success': True,
-        'result': {
-            'sentences': sentence_list
-        },
-        'error': None
-    }
-    return response
+    return response(sentence_list)
+
+
+@app.route("/clean-text", methods=['POST'])
+def clean_text() -> dict:
+    data = request.get_json()
+    text_list = cleaner.clean_text(data['text'])
+    return response(text_list)
 
 
 if __name__ == '__main__':
