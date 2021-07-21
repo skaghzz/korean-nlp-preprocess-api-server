637fb5bad5e9819f9d5a8492716ebec0db2dab7f
namhokim <skaghzz@naver.com>
2021-07-21 17:38:21 +0900

    CORS 제거를 위해서 flask_cors를 추가하고 api에 /api/를 접두어로 추가함

--- app/app.py
+++ app/app.py
@@ -1,9 +1,16 @@
+import json
+
 from flask import Flask, request
+from flask_cors import CORS
 
 from modules.preprocess import split_sentence as splitter
 from modules.preprocess import clean_text as cleaner
 
 app = Flask(__name__)
+CORS(app, resources={
+    r"/v1/*": {"origin": "*"},
+    r"/api/*": {"origin": "*"}
+})
 
 
 def response(result) -> dict:
@@ -15,18 +22,18 @@ def response(result) -> dict:
 
 
 def get_input_texts():
-    data = request.get_json()
-    return data['texts']
+    json_data = request.get_json()
+    return json_data['texts']
 
 
-@app.route("/split-sentence", methods=['POST'])
+@app.route("/api/split-sentence", methods=['POST'])
 def split_sentence() -> dict:
     texts = get_input_texts()
     sentence_list = splitter.split_sentence(texts)
     return response(sentence_list)
 
 
-@app.route("/clean-text", methods=['POST'])
+@app.route("/api/clean-text", methods=['POST'])
 def clean_text() -> dict:
     texts = get_input_texts()
     text_list = cleaner.clean_text(texts)
